b488fef9e2eae1a873b0189fd1df17dca8adcf04
Marco <marcovelardejr@gmail.com>
2022-05-08 20:32:56 -0500

    commit tree 2b61f5727812721898eef6c26d6c9b1394dcccf4

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 const farmAnimals = 'cow horse sheep pig chicken';
 
+const [moo, neigh, baa, oink, cluck] = farmAnimals.split(' ');
+
+const [bessie, ,dolly, babe, little] = farmAnimals.slice('horse').split(' ');
+
+const[blackAndWhite, ,black, pink, ] = farmAnimals.slice('chicken').split(' ');
+
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+
+const[red, orange, yellow, green, blue, indigo, violet] = colors 
+
+const[r, o, y, g, b, ,v] = colors
+
+const[ , , , , ,indg, ] = colors
+
+
+
+ 
+
 
 const muppet = {
   muppetName: 'Miss Piggy',
@@ -24,6 +41,16 @@ const nestedMuppet = {
   nestedJob: 'Host of The Muppet Show',
   nestedPartner: 'Miss Piggy'
 };
+
+
+
+const{ muppetName, color, song, job, partner } = muppet;
+
+const{ song2, song4, } = nestedMuppet.album.theMuppetMovie; 
+
+const{ nestedJob, nestedPartner } = nestedMuppet;
+
+
 
 // Strings
 
